7e8fca25dabfd218b1edf0028bba7e0ac738eb76
ArseniyKolenchenko <ArseniyVFX@gmail.com>
2025-06-20 18:28:37 +0300

    Add .gitignore for macOS and Python artifacts

---
+++ LMI_PointCloud_Baker/__init__.py
@@ -0,0 +1,28 @@
+bl_info = {
+    "name": "LMI PointCloud Baker",
+    "author": "Arseniy Kolenchenko (Lebrov Motion Imagery) & OTOY Inc.",
+    "version": (1, 0, 0),
+    "blender": (4, 4, 3),
+    "location": "View3D > Sidebar > LMI PointCloud Baker",
+    "description": (
+        "Export Geometry Nodes scattered instances to Octane-compatible "
+        "CSVs Scatters and optional instances into Alembic files"
+    ),
+    "category": "Import-Export",
+}
+
+from . import registration
+
+
+def register():
+    """Register all classes and properties."""
+    registration.register()
+
+
+def unregister():
+    """Unregister all classes and properties."""
+    registration.unregister()
+
+
+if __name__ == "__main__":
+    register()
