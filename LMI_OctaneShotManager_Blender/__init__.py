59ba27891b4fa4b6ac09e2f5243a7b56f7d01da9
ArseniyKolenchenko <ArseniyVFX@gmail.com>
2025-07-10 19:16:06 +0200

    Changed name and structure, downgraded version

--- LMI_OctaneShotManager_Blender/__init__.py
+++ LMI_OctaneShotManager_Blender/__init__.py
@@ -1,10 +1,13 @@
 bl_info = {
-    "name": "LMI PointCloud Baker",
-    "author": "Arseniy Kolenchenko (Lebrov Motion Imagery) & OTOY Inc.",
+    "name": "LMI_OctaneShotManager_Blender",
+    "author": "Arseniy Kolenchenko (Lebrov Motion Imagery) & Render Foundation.",
     "version": (1, 1, 0),
     "blender": (4, 4, 3),
-    "location": "View3D > Sidebar > LMI PointCloud Baker",
+    "location": "View3D > Sidebar > LMI Octane ShotManager",
     "description": (
+        "LMI Octane ShotManager for Blender"
+        "Manages TAGs workflow for Octane Render in Blender"
+        "Exports selected collections to separate ORBX files with ability to merge them back to a single scene"
         "Export Geometry Nodes scattered instances to Octane-compatible "
         "CSVs Scatters and optional instances into Alembic files"
     ),
